1524c8795ece9c8b549077213ca14497669ac236
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-04 16:11:40 -0600

    User Story 21 Submit Application, cpomplete

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "show new pet adoption application", type: :feature do
     expect(page).to_not have_content(pet_3.name)
   end
 
-  it "returns matched search results" do
+  it "returns partially matched search results" do
     shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     pet_1 = Pet.create(adoptable: true, age: 7, breed: "sphynx", name: "Bare-y Manilow", shelter_id: shelter.id)
     pet_2 = Pet.create(adoptable: true, age: 3, breed: "domestic pig", name: "Babe", shelter_id: shelter.id)
@@ -44,7 +44,7 @@ RSpec.describe "show new pet adoption application", type: :feature do
     expect(page).to_not have_content(pet_3.name)
   end
 
-  it "returns matched search results" do
+  it "returns case insensitive matched search results" do
     shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     pet_1 = Pet.create(adoptable: true, age: 7, breed: "sphynx", name: "bAre-y Manilow", shelter_id: shelter.id)
     pet_2 = Pet.create(adoptable: true, age: 3, breed: "domestic pig", name: "Babe", shelter_id: shelter.id)
@@ -85,5 +85,31 @@ RSpec.describe "show new pet adoption application", type: :feature do
     expect(page).to_not have_content(pet_3.name)
     expect(page).to_not have_content(pet_3.age)
     expect(page).to_not have_content(pet_3.breed)
+  end
+
+  it "submits the application" do
+    shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: "sphynx", name: "Bare-y Manilow", shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: "domestic pig", name: "Babe", shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: "chihuahua", name: "Elle", shelter_id: shelter.id)
+    application = Application.create!(name: "John Smith", street_address: "321 Main St", city: "Denver", state: "CO", zipcode: 80222)
+
+    visit "/applications/#{application.id}"
+
+    fill_in "Pet Name:", with: "Babe"
+    click_button("Search")
+
+    click_button("Adopt this Pet")
+
+    fill_in "Why I Would Make a Good Pet Owner:", with: "I love animals"
+
+    click_button("Submit Adoption Application")
+    save_and_open_page
+    expect(current_path).to eq("/applications/#{application.id}")
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_2.age)
+    expect(page).to have_content(pet_2.breed)
+    expect(page).to have_content("Pending")
+    expect(page).to_not have_content("Adopt this Pet")
   end
 end
