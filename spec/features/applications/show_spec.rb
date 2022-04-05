943a0a6a53e4ff5ec712355bcaab80cea325048f
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-04 22:55:38 -0600

    User Story 26 Completed Application Show Page, complete

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -96,12 +96,12 @@ RSpec.describe "show new pet adoption application", type: :feature do
 
     visit "/applications/#{application.id}"
 
-    fill_in "Pet Name:", with: "Babe"
+    fill_in :search, with: "Babe"
     click_button("Search")
 
     click_button("Adopt this Pet")
 
-    fill_in "Why I Would Make a Good Pet Owner:", with: "I love animals"
+    fill_in :description, with: "I love animals"
 
     click_button("Submit Adoption Application")
 
@@ -111,5 +111,39 @@ RSpec.describe "show new pet adoption application", type: :feature do
     expect(page).to have_content(pet_2.breed)
     expect(page).to have_content("Pending")
     expect(page).to_not have_content("Adopt this Pet")
+  end
+
+  it "shows a completed application with no text fields or buttons" do
+    shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: "sphynx", name: "Bare-y Manilow", shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: "domestic pig", name: "Babe", shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: "chihuahua", name: "Elle", shelter_id: shelter.id)
+    application = Application.create!(name: "John Smith", street_address: "321 Main St", city: "Denver", state: "CO", zipcode: 80222)
+
+    visit "/applications/#{application.id}"
+
+    fill_in :search, with: "Babe"
+    click_button("Search")
+
+    click_button("Adopt this Pet")
+
+    fill_in :description, with: "I love animals"
+
+    click_button("Submit Adoption Application")
+
+    expect(current_path).to eq("/applications/#{application.id}")
+    expect(page).to have_content("John Smith")
+    expect(page).to have_content("321 Main St")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("CO")
+    expect(page).to have_content(80222)
+    expect(page).to have_content("I love animals")
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_2.age)
+    expect(page).to have_content(pet_2.breed)
+    expect(page).to have_content("Pending")
+    expect(page).to_not have_content("Search")
+    expect(page).to_not have_content("Adopt this Pet")
+    expect(page).to_not have_content("Submit Adoption Application")
   end
 end
